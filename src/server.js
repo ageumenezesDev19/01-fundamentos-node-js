f037114c8796966780b8c17fc3167c611a013108
ageu_dev19 <ageumenezes423@gmail.com>
2024-10-09 19:39:45 -0300

    Create Request Body in JSON: Stream & Buffers

--- src/server.js
+++ src/server.js
@@ -1,9 +1,47 @@
 import http from 'node:http'
 
+// - Criar usuários
+// - Listagem usuários
+// - Edição de usuários
+// - Remoção de usuários
+
+// - HTTP
+//   - Método HTTP
+//   - URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar um recurso do back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+// DELETE => Deletar um recurso do back-end
+
+// GET /users => Buscando usuários no banc-end
+// POST /users => Criar um usuário no back-end
+
+// Stateful - Stateless
+
+// Cabeçalhos (Requisição/resposta) => Metadados
+
+// HTTP Status Code
+
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
   const { method, url } = req
+
+  const buffers = []
+
+  for await (const chunk of req) {
+    buffers.push(chunk)
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString())
+  } catch {
+    req.body = null
+  }
 
   if (method === 'GET' && url === '/users') {
     return res
@@ -12,20 +50,18 @@ const server = http.createServer((req, res) => {
   }
 
   if (method === 'POST' && url === '/users') {
+    const { name, email } = req.body
+
     users.push({
-      id: Math.floor(Math.random() * 1000).toString(),
-      name: 'John Doe',
-      email: 'john.doe@gmail.com',
+      id: 1,
+      name,
+      email,
     })
 
-    return res.writeHead(201).end('Criação de usuário')
+    return res.writeHead(201).end()
   }
 
-  console.log(method, url)
-
-  return res.writeHead(404).end();
+  return res.writeHead(404).end()
 })
 
 server.listen(3333)
-
-// localhost:3333
